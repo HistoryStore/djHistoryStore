062e54b44b76a55481eeb72b7b1bec033977c7d1
Iñaki Cabo Alonso <icaboalo@gmail.com>
2016-04-27 12:19:27 -0500

    deployment fix

--- djHistoryStore/settings/deployment.py
+++ djHistoryStore/settings/deployment.py
@@ -3,7 +3,7 @@ from .base import *
 DEBUG = False
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ('S_KEY')
+SECRET_KEY = os.environ['S_KEY']
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
@@ -11,11 +11,11 @@ SECRET_KEY = os.environ('S_KEY')
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ('DB_NAME'),
-        'USER': os.environ('DB_USER'),
-        'PASSWORD': os.environ('DB_PASSWORD'),
-        'HOST': os.environ('DB_HOST'),
-        'PORT': os.environ('DB_PORT'),
+        'NAME': os.environ['DB_NAME'],
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PASSWORD'],
+        'HOST': os.environ['DB_HOST'],
+        'PORT': os.environ['DB_PORT'],
     }
 }
 
