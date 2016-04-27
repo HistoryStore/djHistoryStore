42d49181b4a6057123a14e4be5c1c34ea5cfa82e
Iñaki Cabo Alonso <icaboalo@gmail.com>
2016-04-27 07:18:59 -0500

    vendor model

--- djHistoryStore/settings/base.py
+++ djHistoryStore/settings/base.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = (
     'rest_framework',
 
     # APPS
-    # 'cadenas',
+    'vendor',
     'productos',
     'compras',
 )
--- vendor/models.py
+++ vendor/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from productos.models import Category
 
 # Create your models here.
+class Vendor(models.Model):
+    class Meta:
+        verbose_name = 'Vendor'
+        verbose_name_plural = 'Vendors'
+
+    class Admin:
+        def __init__(self):
+            pass
+        list_display = ('name', 'address',)
+        ordering = ('name',)
+        search_fields = ('name', 'address')
+
+
+    #Relations
+    categories = models.ManyToManyField(Category)
+
+    #Attributes
+    name = models.CharField(max_length=50, blank=False)
+    address = models.CharField(max_length=150, blank=False)
+    latitude = models.DecimalField(max_digits=11, decimal_places=8, blank=True)
+    longitude = models.DecimalField(max_digits=11, decimal_places=8, blank=True)
